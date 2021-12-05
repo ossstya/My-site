39a7934c65c326087b1a913c0b95dbde539f3e8a
Kostya S <kostyashi88@gmail.com>
2021-12-05 04:57:22 +0200

    Refactor

--- portfoliosite/portfolioapp/urls.py
+++ portfoliosite/portfolioapp/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home_page)
+    path('/h', views.home),
+    path('/works', views.works, name='works'),
+    path('/work/<int:pk>', views.work, name='work'),
+    path('/about', views.about, name='about'),
+    path('', views.contact, name='contact'),
 ]
--- portfoliosite/portfolioapp/views.py
+++ portfoliosite/portfolioapp/views.py
@@ -3,5 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def home_page(request):
+def home(request):
     return render(request, 'portfolioapp/home.html')
+
+
+def works(request):
+    return render(request, 'portfolioapp/works.html')
+
+
+def work(request, pk):
+    return render(request, 'portfolioapp/work.html')
+
+
+def contact(request):
+    return render(request, 'portfolioapp/contact.html')
+
+
+def about(request):
+    return render(request, 'portfolioapp/about.html')
