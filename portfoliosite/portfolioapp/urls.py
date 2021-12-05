7022a3c9f7221c622e96c56ff5b9366d27bdd55a
Kostya S <kostyashi88@gmail.com>
2021-12-05 05:56:25 +0200

    Final setup

--- portfoliosite/portfolioapp/urls.py
+++ portfoliosite/portfolioapp/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('/h', views.home),
-    path('/works', views.works, name='works'),
-    path('/work/<int:pk>', views.work, name='work'),
-    path('/about', views.about, name='about'),
-    path('', views.contact, name='contact'),
+    path('', views.home, name='home'),
+    path('works/', views.works, name='works'),
+    path('work/<int:pk>', views.work, name='work'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
 ]
