386cd3c27cdf5687615776a7c6f2a9220bd0d733
Kostya S <kostyashi88@gmail.com>
2021-12-21 14:22:19 +0200

    Add requirements and models

--- portfoliosite/portfolioapp/models.py
+++ portfoliosite/portfolioapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Work(models.Model):
+    title = models.CharField(max_length=50)
+    body = models.TextField()
+    title_image = models.ImageField()
+    first_body_image = models.ImageField()
+    second_body_image = models.ImageField()
+    third_body_image = models.ImageField()
+    body_title = models.TextField()
+
+
+class Contact(models.Model):
+    email = models.EmailField()
+    subject = models.CharField(max_length=200)
+    message = models.TextField()
+
+
+class Tag(models.Model):
+    pass
