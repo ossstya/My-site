365daf691fe94f4b31cb146dfa094bb3a23c7bed
Kostya S <kostyashi88@gmail.com>
2021-12-23 16:42:41 +0200

    Add new fild to models

--- portfoliosite/portfolioapp/models.py
+++ portfoliosite/portfolioapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Work(models.Model):
     title = models.CharField(max_length=150)
-    # title_of_card = models.CharField(max_length=100)
+    title_of_card = models.CharField(max_length=100)
     body = models.TextField()
     title_image = models.ImageField(upload_to='work_title_image/',
                                     null=True,
